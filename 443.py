99e6afb66a7c4c61020512090fd8db795d24f080
Raghav Mishra <24ADV3ARI0073@sageuniversity.in>
2026-02-05 10:32:46 +0530

    commit 12

--- 443.py
+++ 443.py
@@ -1,5 +1,19 @@
 '''
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+
+Note: The characters in the array beyond the returned length do not matter and should be ignored.
 '''
+
 
 class Solution(object):
     def compress(self, chars):
@@ -9,19 +23,21 @@ class Solution(object):
         """
         insert = 0
 
-        i = 0 
+        i = 0
 
         while i < len(chars):
             group = 1
-            while (group +i) < len(chars) and chars[group +i] == chars[i]:
-                group +=1
+            while (group + i) < len(chars) and chars[group + i] == chars[i]:
+                group += 1
 
             chars[insert] = chars[i]
-            insert +=1
+            insert += 1
             if group > 1:
                 string = str(group)
-                chars[insert : insert + len(string )] = list(string)
+                chars[insert : insert + len(string)] = list(string)
                 insert += len(string)
 
-            i += group 
-        return insert 
+            i += group
+        return insert
+# time complexity is O(n)
+# space complexity is O(1)

8f05dbc556cda0fcae48888b28d062f6de708435
Raghav Mishra <24ADV3ARI0073@sageuniversity.in>
2026-02-07 10:43:35 +0530

    commit 22

--- 1657.py
+++ 1657.py
@@ -1,3 +1,16 @@
+
+'''
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+'''
+
 class Solution(object):
     def closeStrings(self, word1, word2):
         """
@@ -6,8 +19,12 @@ class Solution(object):
         :rtype: bool
         """
         if len(word1) != len(word2):
-            return False 
+            return False
 
         counts1 = Counter(word1)
-        counts2=  Counter(word2)
-        return ( counts1.keys() == counts2.keys() ) and (sorted (counts1.values()) == sorted (counts2.values()))
+        counts2 = Counter(word2)
+        return (counts1.keys() == counts2.keys()) and (
+            sorted(counts1.values()) == sorted(counts2.values()))
+
+# time complexity is O(nlogn)
+# space complexity is O(n)
